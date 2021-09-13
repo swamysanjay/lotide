92e03dc425027a880e9d2ef4f1d74b4ded3cdc76
Sanjay Swamy <swamysanjay98@gmail.com>
2021-09-13 20:13:00 +0000

    test for end of array

---
+++ tail.js
@@ -0,0 +1,17 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const tail = function(array) {
+  return array.slice(1);
+};
+
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
