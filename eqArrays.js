108273a542878997f18d1b62a951b97f04d5d52d
Sanjay Swamy <swamysanjay98@gmail.com>
2021-09-14 21:56:38 +0000

    made an eqArrays.js file

---
+++ eqArrays.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if ((Array.isArray(array1[i]) || Array.isArray(array2[i])) && eqArrays(array1[i], array2[i]) === false) {
+      return false;
+    }
+    else if (Array.isArray(array1[i]) || Array.isArray(array2[i])) {
+      eqArrays(array1[i], array2[i]);
+    }
+    else if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+  return true;
+
+}
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false);
+
