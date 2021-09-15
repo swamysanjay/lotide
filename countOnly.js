8aaf967e5c7316e55019bfbbf84004ffb4703936
Sanjay Swamy <swamysanjay98@gmail.com>
2021-09-15 23:11:31 +0000

    created a countOnly.js

---
+++ countOnly.js
@@ -0,0 +1,42 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`)
+  }
+
+};
+
+const countOnly = function(allItems, itemsToCount) {
+  let items = {};
+  for (let i = 0; i < allItems.length; i++) {
+    if (itemsToCount[allItems[i]]) {
+      if (items[allItems[i]]) {
+        items[allItems[i]]++;
+      } else {
+        items[allItems[i]] = 1;
+      }
+
+    }
+  }
+  return items;
+}
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
