0a50cd0da869b8274cde995d18f246b83c790a54
Sanjay Swamy <swamysanjay98@gmail.com>
2021-09-16 00:55:58 +0000

    Adding eqObjects file into lotide folder

--- eqObjects.js
+++ eqObjects.js
@@ -2,7 +2,7 @@ const assertEqual = function(actual, expected) {
   if (actual === expected) {
     console.log(`Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`)
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
@@ -15,12 +15,12 @@ const eqObjects = function(object1, object2) {
   for (let key in object1) {
     if (Array.isArray(object1[key]) && Array.isArray(object2[key])) {
       return eqObjects(object1[key], object2[key]);
-    } else if((typeof(object1[key]) !== typeof({}) && typeof(object2[key]) !== typeof({})) && (object1[key] !== object2[key])) {
-        return false;
+    } else if ((typeof(object1[key]) !== typeof({}) && typeof(object2[key]) !== typeof({})) && (object1[key] !== object2[key])) {
+      return false;
     } else if ((typeof(object1[key]) === typeof({}) || typeof(object1[key]) === typeof({})) && eqObjects(object1[key], object2[key]) === false) {
-        return false;
+      return false;
     } else if (typeof(object1[key]) === typeof({}) || typeof(object1[key]) === typeof({})) {
-        return false;
+      return false;
     }
   }
   return true;
