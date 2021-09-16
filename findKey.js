44812297bb29f1b6988f00ed25f7d9c49b2c0e22
Sanjay Swamy <swamysanjay98@gmail.com>
2021-09-16 19:46:07 +0000

    created a findKey.js file

---
+++ findKey.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`)
+  }
+
+};
+
+const findKey = function (object, callback) {
+  let results = [];
+  for (let item in object) {
+    if (callback(object[item])) {
+      return item;
+    }
+  }
+}
+
+findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)
+
