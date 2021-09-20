179ab98c59a029df529c3692e8f006efe0f961aa
Sanjay Swamy <swamysanjay98@gmail.com>
2021-09-20 15:12:51 +0000

    created test file for middle.js and refactored middle.js

--- middle.js
+++ middle.js
@@ -44,9 +44,4 @@ const middle = function (array) {
 
 
 
-console.log(middle([1])) // => []
-console.log(middle([1, 2]))
-console.log(middle([1, 2, 3])) // => [2]
-console.log(middle([1, 2, 3, 4, 5])) // => [3]
-console.log(middle([1, 2, 3, 4])) // => [2, 3]
-console.log(middle([1, 2, 3, 4, 5, 6])) // => [3, 4]
+module.exports = middle;
