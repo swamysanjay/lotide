403f66edcf48d0c2c7601ebae7e51a1caa6a4e9e
Sanjay Swamy <swamysanjay98@gmail.com>
2021-09-20 15:24:19 +0000

    changed test code to use mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,4 +1,21 @@
-const tail = require('../tail');
+const assert = require('chai').assert;
+const tail   = require('../tail');
+
+describe("#tail", () => {
+  const words = ["Yo Yo", "Lighthouse", "Labs"];
+  it("returns 3 for words.length", () => {
+    assert.strictEqual((words.length), 3);
+  });
+  it("returns 2 for tail(words).length", () => {
+    assert.strictEqual(tail(words).length, 2);
+  });
+  it("returns ['Lighthouse', 'Labs'] for tail(words)", () => {
+    assert.deepEqual(tail(words), ['Lighthouse', 'Labs']);
+  });
+});
+
+//Original Test Code
+/*const tail = require('../tail');
 const assertEqual = require('../assertEqual');
 
 // Test Case: Check the original array
@@ -11,4 +28,4 @@ if(true) {
   x + 2;
   x = 4;
 }
-console.log(x);
+console.log(x); */
